530771bd9b65da2e5a25fa124569903a9ff223a2
Mikkel Pedersen <mikk79h6@zbc.dk>
2021-08-30 13:40:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_Opgave
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            // Her opretter vi vores første kvadrat med en side på 5
+            Square kvadrat = new Square(5);
+            Console.WriteLine(kvadrat.Perimiter());
+            Console.WriteLine(kvadrat.Area());
+
+            // Her opretter vi vores anden kvadrat med en side på 10
+            Square kvadrat2 = new Square(10);
+            Console.WriteLine(kvadrat2.Perimiter());
+            Console.WriteLine(kvadrat2.Area());
+            
+            // Her opretter vi vores tredje kvadrat fra en tom constructor og giver først siden en værdi efter den er oprettet
+            Square kvadrat3 = new Square();
+            kvadrat3.Side = 15;
+            Console.WriteLine(kvadrat3.Perimiter());
+            Console.WriteLine(kvadrat3.Area());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_Opgave
+{
+    public class Square
+    {
+        // Opgave A og B
+
+        // Attribute til vores firkant
+        private int side;
+
+        public int Side
+        {
+            get
+            {
+                return this.side;
+            }
+            set
+            {
+                this.side = value;
+            }
+        }
+
+        // Constructor som ikke kræver en værdi
+        public Square()
+        {
+        }
+
+        // Constructer som kræver en værdi
+        public Square(int a)
+        {
+            this.side = a;
+        }
+
+        // Metode til at udregne omkreds
+        public int Perimiter()
+        {
+            return (4 * side);
+        }
+        // Metode til at udregne areal
+        public int Area()
+        {
+            return (side * side);
+        }
+    }
+}
